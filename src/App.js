64041fae6733372a19d731849ba09f1d7bedd3d6
cvonwilczur <cvonwilczur@gmail.com>
2018-08-01 09:36:43 -0700

    commit for general frontend components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 import Navigation from './components/navigation/Navigation'
-import Logo from './components/logo/Logo'
+import Particles from 'react-particles-js';
+import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/imagelinkform/ImageLinkForm'
+import Rank from './components/rank/Rank'
+
 
 import './App.css';
+
+const particlesOptions= {
+  particles: {
+    line_linked: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800
+        },
+      }
+    }
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+             params={particlesOptions}
+           />
         <Navigation />
         <Logo />
-        {/* <ImageLinkForm />
-        <FaceRecognition /> */}
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
 
       </div>
     );
